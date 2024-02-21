a1a90d534be2225e690bf9aee81a8e69e548ce49
Roshan Jambhulkar <roshanyjambhulkar1204@gmail.com>
2024-02-21 11:01:03 +0530

    added project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,48 +1,53 @@
 export const projects = [
   {
-    title: 'Desertcart clone' ,
-    description: "A website that sells lifestyle products.",
-      image: '/images/desertcart.png',
-      tags: ['HTML', 'CSS', 'JavaScript'],
-    source: 'https://github.com/vsrathod39/desertcart.in_clone.io',
-    visit: 'https://vsrathod39.github.io/desertcart.in_clone.io/',
+    title: "Desertcart clone",
+    description:
+      "A website that sells lifestyle products. Desertcart provides a seamless and secure shopping platform with 100 million+ products from around the globe delivered to your door.",
+    image: "/images/desertcart.png",
+    tags: ["HTML", "CSS", "JavaScript"],
+    source: "https://github.com/vsrathod39/desertcart.in_clone.io",
+    visit: "https://vsrathod39.github.io/desertcart.in_clone.io/",
     id: 0,
   },
   {
-    title: 'Myntra clone',
-    description:"A website that sells lifestyle products.",
-    image: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1636821131818/fsR2vxcX6.png?auto=compress,format&format=webp',
-    tags: ['HTML', 'CSS', 'JavaScript'],
-    source: 'https://github.com/nkil123/MyntraClone',
-    visit: 'https://mysterious-depths-64439.herokuapp.com/',
+    title: "Myntra clone",
+    description:
+      "Online Shopping Site for Fashion & Lifestyle in India. India's Fashion Expert brings you a variety of footwear, Clothing, Accessories and lifestyle products.",
+    image:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1636821131818/fsR2vxcX6.png?auto=compress,format&format=webp",
+    tags: ["HTML", "CSS", "JavaScript"],
+    source: "https://github.com/nkil123/MyntraClone",
+    visit: "https://myntra-clone-xi.vercel.app/",
     id: 1,
   },
   {
-    title: 'Myntra clone',
-    description:"A website that sells lifestyle products.",
-    image: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1636821131818/fsR2vxcX6.png?auto=compress,format&format=webp',
-    tags: ['HTML', 'CSS', 'JavaScript', "Express"],
-    source: 'https://github.com/nkil123/MyntraFullStack',
-    visit: 'https://mysterious-depths-64439.herokuapp.com/',
+    title: "Policy bazaar",
+    description:
+      "Compare Insurance Policies — Compare Quotes from Top Insurers. Easy Online Process & Get PolicyBazaar Expert Advice Now. One-Stop Destination For All Your Online Insurance & Financial Needs.",
+    image:
+      "https://user-images.githubusercontent.com/89926084/150676449-9bbd6976-ccdf-48be-b2c6-4275a029134c.png",
+    tags: ["HTML", "CSS", "JavaScript", "React", "Redux", "Express"],
+    source: "https://github.com/Renukote/policy-bazaar-clone",
+    visit: "https://policybazar.vercel.app/",
     id: 1,
   },
-  
+
   {
-    title: 'Medium clone',
-    description:"A platform where undiscovered voices can share their writing on any topic.",
-    image: 'images/medium.png',
-    tags: ['HTML', 'CSS', 'JavaScript', "React", "Redux"],
-    source: 'https://github.com/Roshanswork/Medium-clone',
-    visit: 'https://medium-clone-gules.vercel.app/',
+    title: "Medium clone",
+    description:
+      "Medium is an open platform where readers find dynamic thinking, and where expert and undiscovered voices can share their writing on any topic.",
+    image: "images/medium.png",
+    tags: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    source: "https://github.com/Roshanswork/Medium-clone",
+    visit: "https://medium-clone-gules.vercel.app/",
     id: 1,
-  }
-  
-]
+  },
+];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
 ];
